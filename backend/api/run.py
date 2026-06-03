ab0c402fec766d1cbb7df31ca06550dd769e0c03
tomvieilledent <tomvieilledent@gmail.com>
2026-06-03 15:55:58 +0200

    Change dev server port from 5000 to 8000

--- backend/api/run.py
+++ backend/api/run.py
@@ -9,4 +9,4 @@ from backend.api.app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
